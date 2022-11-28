6637cd54962b3dbfe52050f721c0c6afb4f9d438
Yoobit <yoobit@yahoo.com>
2022-11-27 21:28:23 -0500

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+const tokml = require('tokml');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'test-project';
+  geoObject = {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [125.6, 10.1],
+    },
+    properties: {
+      name: 'Dinagat Islands',
+    },
+  };
+  kmlObject = tokml(this.geoObject);
+  ngOnInit() {
+    console.log(this.kmlObject);
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      let loader = document.getElementById('loader');
+      if (loader != null) loader.style.display = 'none';
+    }, 3000);
+
+  }
 }
